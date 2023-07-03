3a26514b0343d208511b21b87f3c68837aa49712
Brian <134489011+Brikita@users.noreply.github.com>
2023-07-03 16:23:16 +0300

    commit 6  linking to my database

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express();
+const mongoose = require('mongoose')
 const morgan = require("morgan");
+const dotenv = require('dotenv');
+dotenv.config();
+
+// db
+mongoose.connect(
+    process.env.MONGO_URI,
+    {useNewUrlParser: true}
+    )
+    .then(() => console.log("DB Connnected"))
+
+mongoose.connection.on("error", err => {
+    console.log(`DB connection error: ${err.message}`);
+})
+
 
  // bring in routes
  const postRoutes = require('./routes/post')
